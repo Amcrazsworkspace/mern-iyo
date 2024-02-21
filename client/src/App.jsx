5ed5f8df9ff96ba276854516ee4ba8dcff740b7a
amcrazseworkspace <amcrazseworkspace@gmail.com>
2024-02-21 18:42:03 +0530

    create header components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,15 +4,17 @@ import Signin from "./Pages/Signin";
 import Signout from "./Pages/Signout";
 import About from "./Pages/About";
 import Profile from "./Pages/Profile";
+import Header from "./components/header";
 
 export default function App() {
   return <BrowserRouter>
+  <Header/>
   <Routes>
-  <Route path="/"element={<Home />} />
-  <Route path="/signin"element={<Signin />} />
-  <Route path="/signout"element={<Signout />} />
-  <Route path="/about"element={<About />} />
-  <Route path="/profile"element={<Profile />} />
+  <Route path='/'element={<Home />} />
+  <Route path='/signin'element={<Signin />} />
+  <Route path='/signout'element={<Signout />} />
+  <Route path='/about'element={<About />} />
+  <Route path='/profile'element={<Profile />} />
   </Routes>
   </BrowserRouter>;
 }
